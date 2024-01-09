d44f17a2ad033a5b512bead91c1030037fb0885e
Aarish Khan <154256796+aarish47@users.noreply.github.com>
2024-01-09 23:25:09 +0500

    Update Machine-Learning-Application.py

--- Machine-Learning-Application.py
+++ Machine-Learning-Application.py
@@ -81,7 +81,7 @@ def get_classifier(classifier_name, params):
                                            max_depth=params['max_depth'], random_state=1234)
     return clf
  
-if st.button('Check the code!') :
+if st.selectbox('Check the code!') :
     with st.echo():
         clf = get_classifier(classifier_name, params)
 
